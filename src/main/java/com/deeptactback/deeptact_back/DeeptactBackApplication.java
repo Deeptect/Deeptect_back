3d2861e8460efedc71ea5173b9a03c3a6cc5dc42
hhjkhhbgkugjnbj <ck8901ck@gmail.com>
2025-03-23 17:28:30 +0900

    Create

---
+++ src/main/java/com/deeptactback/deeptact_back/DeeptactBackApplication.java
@@ -0,0 +1,13 @@
+package com.deeptactback.deeptact_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeptactBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeeptactBackApplication.class, args);
+	}
+
+}
