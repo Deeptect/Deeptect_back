232887aecbd4c900b2db74b7553ef596aabc95f7
hhjkhhbgkugjnbj <ck8901ck@gmail.com>
2025-04-10 18:16:36 +0900

    Feat: 비디오 정보 저장 구현

--- src/main/java/com/deeptactback/deeptact_back/service/CloudflareR2ServiceImpl.java
+++ src/main/java/com/deeptactback/deeptact_back/service/CloudflareR2ServiceImpl.java
@@ -9,7 +9,6 @@ import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
